a55bef8c755874524748e5edbe5cbf4d5fad47b8
Mateus Gouvea <falamateus.gouvea@gmail.com>
2025-01-29 16:22:26 -0300

    chore(Create): Components and Constants
    - Button
    - Footer
    - Navbar

---
+++ components/Button.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type?: "button" | "submit" | "reset";
+  title: string;
+  icon?: string;
+  variant?:
+    | "btn_white"
+    | "btn_white_text"
+    | "btn_green"
+    | "btn_dark_green"
+    | "btn_dark_green_outline";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
