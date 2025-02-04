39182a36a2cd8591a412194d6db40857df3c2195
Mateus Gouvea <falamateus.gouvea@gmail.com>
2025-02-04 13:04:59 -0300

    chore(Button Component): Add full props

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,13 +10,16 @@ type ButtonProps = {
     | "btn_green"
     | "btn_dark_green"
     | "btn_dark_green_outline";
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
